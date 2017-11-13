752dfbc05b90f7a46268df1192226c46e63df074
ZhenyaVoronkin <john.voronkin@gmail.com>
2017-11-13 22:01:45 +0300

    +

--- src/test/java/ru/simple/java/test/anonymousInnerClassesAndLambda/lambda/LambdaTest.java
+++ src/test/java/ru/simple/java/test/anonymousInnerClassesAndLambda/lambda/LambdaTest.java
@@ -1,4 +1,27 @@
 package ru.simple.java.test.anonymousInnerClassesAndLambda.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class LambdaTest {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 5, 67, 12);
+
+        numbers.forEach(System.out::println);
+        System.out.println("\nсумма чётных чисел: " + sumAll(numbers, n -> n % 2 == 0));
+    }
+
+    private static int sumAll(List<Integer> numbers, Predicate<Integer> p) {
+        int total = 0;
+        for (int number : numbers) {
+            if (p.test(number)) {
+                total += number;
+            }
+        }
+        return total;
+    }
+
+
 }

54e5cef21055d13c7d8e3a29233918d20614b211
ZhenyaVoronkin <john.voronkin@gmail.com>
2018-01-07 22:54:30 +0300

    -added constant

--- my.simple.java/src/test/java/ru/simple/java/test/List/PerformanceArrayListLinkedListTest.java
+++ my.simple.java/src/test/java/ru/simple/java/test/List/PerformanceArrayListLinkedListTest.java
@@ -9,51 +9,55 @@ public class PerformanceArrayListLinkedListTest {
         ArrayList<Integer> arrayList = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
 
-    // ArrayList add
+        final int I_100000 = 100_000;
+        final int I_9999 = 9999;
+
+        // ArrayList add
         long startTime = System.nanoTime();
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < I_100000; i++) {
             arrayList.add(i);
         }
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         System.out.println("ArrayList add:  " + duration);
 
-    // LinkedList add
+        // LinkedList add
         startTime = System.nanoTime();
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < I_100000; i++) {
             linkedList.add(i);
         }
         endTime = System.nanoTime();
         duration = endTime - startTime;
         System.out.println("LinkedList add: " + duration);
 
-    // ArrayList get
+        System.out.println("===============");
+        // ArrayList get
         startTime = System.nanoTime();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < I_100000; i++) {
             arrayList.get(i);
         }
         endTime = System.nanoTime();
         duration = endTime - startTime;
         System.out.println("ArrayList get:  " + duration);
 
-    // LinkedList get
+        // LinkedList get
         startTime = System.nanoTime();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < I_100000; i++) {
             linkedList.get(i);
         }
         endTime = System.nanoTime();
         duration = endTime - startTime;
         System.out.println("LinkedList get: " + duration);
 
-
-    // ArrayList remove
+        System.out.println("===============");
+        // ArrayList remove
         startTime = System.nanoTime();
 
-        for (int i = 9999; i >= 0; i--) {
+        for (int i = I_9999; i >= 0; i--) {
             arrayList.remove(i);
         }
         endTime = System.nanoTime();
@@ -61,10 +65,10 @@ public class PerformanceArrayListLinkedListTest {
         System.out.println("ArrayList remove:  " + duration);
 
 
-    // LinkedList remove
+        // LinkedList remove
         startTime = System.nanoTime();
 
-        for (int i = 9999; i >= 0; i--) {
+        for (int i = I_9999; i >= 0; i--) {
             linkedList.remove(i);
         }
         endTime = System.nanoTime();

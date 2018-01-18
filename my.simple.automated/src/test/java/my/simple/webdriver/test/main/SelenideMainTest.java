83271d4413adfd5ce3ece75e555ee0e5cb900653
john.voronkin <testinglife.j@gmail.com>
2018-01-18 13:31:47 +0300

    -Selenide vs clear Selenium

--- my.simple.automated/src/test/java/my/simple/webdriver/test/main/SelenideMainTest.java
+++ my.simple.automated/src/test/java/my/simple/webdriver/test/main/SelenideMainTest.java
@@ -1,12 +1,13 @@
 package my.simple.webdriver.test.main;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideMainTest {
@@ -20,7 +21,12 @@ public class SelenideMainTest {
     public void openPage() {
         open("http://mvnrepository.com");
         assertThat(title())
-                .isEqualTo("Maven Reposiory: Search/Browse/Explore");
+                .isEqualTo("Maven Repository: Search/Browse/Explore");
+        $x("//*[@id='query']").setValue("gradle");
+        $x("//*[@id='search']/form/input[2]").click();
+        $x("//*[@id='maincontent']/div[3]/div[1]/h2/a[1]").shouldHave(Condition.exactText("Gradle Core"));
+        $$x("//a[contains(text(), 'gradle')]")
+                .shouldHave(size(16));
     }
 
 }

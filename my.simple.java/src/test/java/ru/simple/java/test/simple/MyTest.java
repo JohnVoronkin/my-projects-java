847e3621713d1ab1c77f62b1ce04b6f22e3b7f6c
zhenyavoronkin <voronkin@gorparkovki.ru>
2018-08-03 10:43:27 +0300

    init

--- my.simple.java/src/test/java/ru/simple/java/test/simple/MyTest.java
+++ my.simple.java/src/test/java/ru/simple/java/test/simple/MyTest.java
@@ -1,23 +1,46 @@
 package ru.simple.java.test.simple;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class MyTest {
-    public static void main(String[] args) {
-        System.out.println(Arrays.stream(new String[]{"a", "b", "c"})
-                .collect(Collectors.joining(",")));
 
-        System.out.println(max(1,9));
-        System.out.println(max(3,2));
+  public static void main(String[] args) {
+    A a = new B(1);
+  }
+}
 
+class A {
+  A() {
+    System.out.println("A");
+  }
 
-    }
+  {
+    System.out.println("AC");
+  }
 
-    private static int max(int arg1, int arg2){
-        if(arg1 < arg2) return arg1;
-        return arg2;
+  static {
+    System.out.println("AK");
+  }
 
-    }
+}
 
+class B extends A {
+  B(int i) {
+    this();
+    System.out.println("B");
+  }
+
+  {
+    System.out.println("BC");
+  }
+
+  B() {
+    System.out.println("def B");
+  }
+
+  {
+    System.out.println("BC2");
+  }
+
+  static {
+    System.out.println("BK");
+  }
 }

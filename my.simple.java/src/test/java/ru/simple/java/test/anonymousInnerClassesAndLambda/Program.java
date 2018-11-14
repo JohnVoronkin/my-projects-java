cd38152da28d8eb709c06994b3a55d4ffed281df
zhenyavoronkin <voronkin@gorparkovki.ru>
2018-11-14 11:33:27 +0300

    mini style edits

--- my.simple.java/src/test/java/ru/simple/java/test/anonymousInnerClassesAndLambda/Program.java
+++ my.simple.java/src/test/java/ru/simple/java/test/anonymousInnerClassesAndLambda/Program.java
@@ -8,22 +8,22 @@ import static java.lang.System.out;
 
 public class Program {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Switcher switcher = new Switcher();
+    Switcher switcher = new Switcher();
 
-        // подписка на событие
-        switcher.addElectricityListener(new Lamp());
-        switcher.addElectricityListener(new Radio());
+    // подписка на событие
+    switcher.addElectricityListener(new Lamp());
+    switcher.addElectricityListener(new Radio());
 
         /*
         Анонимный класс - включает в себя описание класса (как такового) и создание объекта данного
         класса
          */
-        switcher.addElectricityListener(() -> out.println("Fire!")); // Лямбда-выражения (фактически - использование объекта, как функции)
+    switcher.addElectricityListener(() -> out.println("Fire!")); // Лямбда-выражения (фактически - использование объекта, как функции)
 
-        switcher.switchOn();
+    switcher.switchOn();
 
-    }
+  }
 
 }
--- my.simple.java/src/test/java/ru/simple/java/test/arrays/ArrayListTest.java
+++ my.simple.java/src/test/java/ru/simple/java/test/arrays/ArrayListTest.java
@@ -4,48 +4,48 @@ import java.util.ArrayList;
 
 public class ArrayListTest {
 
-    public static void main(String[] args) {
-        System.out.println(LIST);
+  public static void main(String[] args) {
+    System.out.println(LIST);
 
-        // create an empty array list with an initial capacity
-        ArrayList<Integer> arrlist = new ArrayList<Integer>(5);
+    // create an empty array list with an initial capacity
+    ArrayList<Integer> arrlist = new ArrayList<Integer>(5);
 
-        // use add() method to add elements in the list
-        arrlist.add(35);
-        arrlist.add(20);
-        arrlist.add(25);
+    // use add() method to add elements in the list
+    arrlist.add(35);
+    arrlist.add(20);
+    arrlist.add(25);
 
-        // Trim the arraylist
-        arrlist.trimToSize();
+    // Trim the arraylist
+    arrlist.trimToSize();
 
-        System.out.printf("В списке %s эл. \n", arrlist.size());
-        // let us print all the elements available in list
-        for (Integer number : arrlist) {
-            System.out.println("Number = " + number);
-        }
-
-        System.out.println("Операция удаления:");
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.removeAll(list.subList(2, 5));
-        list.trimToSize();
-        System.out.println(list); //[1, 2, 6, 7, 8]
+    System.out.printf("В списке %s эл. \n", arrlist.size());
+    // let us print all the elements available in list
+    for (Integer number : arrlist) {
+      System.out.println("Number = " + number);
     }
 
-    final static ArrayList<String> LIST = new ArrayList<String>() {
-        {
-            add("value one");
-            add("value two");
-            add("value there");
-        }
-    };
+    System.out.println("Операция удаления:");
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    list.add(7);
+    list.add(8);
+    list.removeAll(list.subList(2, 5));
+    list.trimToSize();
+    System.out.println(list); //[1, 2, 6, 7, 8]
+  }
+
+  final static ArrayList<String> LIST = new ArrayList<String>() {
+    {
+      add("value one");
+      add("value two");
+      add("value there");
+    }
+  };
 
 
 }

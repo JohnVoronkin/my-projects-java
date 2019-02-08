344d94342fbc196f8276b1783e25c04ac0f9b45c
zhenyavoronkin <voronkin@gorparkovki.ru>
2019-02-08 14:50:55 +0300

    mini fix

--- src/test/java/ru/simple/java/test/BaseTest.java
+++ src/test/java/ru/simple/java/test/BaseTest.java
@@ -3,17 +3,23 @@ package ru.simple.java.test;
 public class BaseTest {
 
   private final static String RED = "красный";
+  private final static String YELLOW = "желтый";
+  private final static String GREEN = "зеленый";
   private final static String BLUE = "синий";
 
   public static void main(String[] args) {
 
-    new BaseTest().freeSpacesParking(19);
+    new BaseTest().freeSpacesParking(49);
 
   }
 
-  private void freeSpacesParking(final int ratio) {
-    if (ratio >= 0 && ratio < 20) {
+  private void freeSpacesParking(final Integer ratio) {
+    if (ratio >= 0 && ratio <= 20)
       System.out.println(RED);
-    } else System.out.println(BLUE);
+    else if (ratio > 20 && ratio <= 49)
+      System.out.println(YELLOW);
+    else if (ratio >= 50)
+      System.out.println(GREEN);
+    else System.out.println(BLUE);
   }
 }

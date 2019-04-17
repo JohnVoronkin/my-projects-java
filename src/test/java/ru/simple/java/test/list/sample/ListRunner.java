63907d3f7e6067beac2109dd357ed7c8bd3f126a
evgeniy.voronkin <evgeniy.voronkin@gorparkovki.ru>
2019-04-17 14:53:01 +0300

    add list

--- src/test/java/ru/simple/java/test/list/sample/ListRunner.java
+++ src/test/java/ru/simple/java/test/list/sample/ListRunner.java
@@ -1,4 +1,9 @@
 package ru.simple.java.test.list.sample;
 
 public class ListRunner {
+
+
+  public static void main(String[] args) {
+
+  }
 }
--- src/test/java/ru/simple/java/test/list/sample/ToDoList.java
+++ src/test/java/ru/simple/java/test/list/sample/ToDoList.java
@@ -1,4 +1,44 @@
 package ru.simple.java.test.list.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * List - это order collection (упорядоченная коллекция). Добавили в 3-й (в конец списка) индекс по списку, остается третьим и т.д.
+ */
 public class ToDoList {
+
+  private List<String> toDoList = new ArrayList<>();
+
+
+  public void addToList(final String task) {
+    toDoList.add(task);
+  }
+
+  public void addToListatPosition(final int position, final String task) {
+    toDoList.add(position, task);
+  }
+
+  public void printToDoList() {
+    int count = 0;
+    for (String task : toDoList) {
+      System.out.println((count++) + " " + task);
+    }
+  }
+
+  public void changeTask(final int index, final String task) {
+    // set - позволяет перезаписать элемент в списке
+    toDoList.set(index, task);
+  }
+
+  public void removeTask(final String task) {
+    toDoList.remove(task);
+  }
+
+  public void getTaskPriority(final String task) {
+    // indexOf - получить индекс элемента списка
+    toDoList.indexOf(task);
+  }
+
+
 }

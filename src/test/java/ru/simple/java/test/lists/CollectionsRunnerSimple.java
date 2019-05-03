b1d7350dcb31831b68555cc30ea90b435a2d917b
zhenya voronkin <zhenya.voronkin@gmail.com>
2019-05-03 15:16:02 +0300

    compare

--- src/test/java/ru/simple/java/test/lists/CollectionsRunnerSimple.java
+++ src/test/java/ru/simple/java/test/lists/CollectionsRunnerSimple.java
@@ -1,12 +1,49 @@
 package ru.simple.java.test.lists;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CollectionsRunnerSimple {
 
   public static void main(String[] args) {
+    List<Card> deckOfCards = new ArrayList<>();
 
+    for (Card.Face face : Card.Face.values()) {
+      for (Card.Suit suit : Card.Suit.values()) {
+        deckOfCards.add(new Card(suit, face));
+      }
+    }
+
+    System.out.println("Original deck of cards");
+    for (int i = 0; i < deckOfCards.size(); i++) {
+      System.out.printf("%-15s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+    }
+    Collections.shuffle(deckOfCards);
+
+    System.out.println("\n\ncards auto shuffle");
+    for (int i = 0; i < deckOfCards.size(); i++) {
+      System.out.printf("%-15s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+    }
+
+    Collections.sort(deckOfCards);
+
+    System.out.println("\n\ncards after sorted");
+    for (int i = 0; i < deckOfCards.size(); i++) {
+      System.out.printf("%-15s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+    }
+
+    Collections.sort(deckOfCards, Collections.reverseOrder());
+
+    System.out.println("\n\ncards after sorted of reverse order");
+    for (int i = 0; i < deckOfCards.size(); i++) {
+      System.out.printf("%-15s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+    }
+
+    Collections.sort(deckOfCards, new Card.CardComparator());
+
+    System.out.println("\n\ncards after sorted custom Comparator");
+    for (int i = 0; i < deckOfCards.size(); i++) {
+      System.out.printf("%-15s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+    }
   }
 
   /*
@@ -30,6 +67,14 @@ public class CollectionsRunnerSimple {
       return face;
     }
 
+    enum Face {
+      Ace, Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King;
+    }
+
+    enum Suit {
+      SPADES, HEARTS, CLUBS, DIMONDS
+    }
+
     @Override
     public int compareTo(Card card) {
       List<Face> faceList = Arrays.asList(Face.values());
@@ -37,20 +82,40 @@ public class CollectionsRunnerSimple {
       if (faceList.indexOf(this.face) < faceList.indexOf(card.getFace())) {
         return -1;
       } else if (faceList.indexOf(this.face) > faceList.indexOf(card.getFace())) {
-        return +1;
+        return 1;
       } else if (faceList.indexOf(this.face) == faceList.indexOf(card.getFace())) {
         return String.valueOf(this.suit).compareTo(String.valueOf(card.getSuit()));
       }
       return 0;
     }
+
+    /*@Override
+    public int compareTo(Card o) {
+      return 0;
+    }*/
+
+    static class CardComparator implements Comparator<Card> {
+      List<Face> faceList = Arrays.asList(Face.values());
+
+
+      @Override
+      public int compare(Card card1, Card card2) {
+        if (faceList.indexOf(card1.getFace()) < faceList.indexOf(card2.getFace())) {
+          return 1;
+        } else if (faceList.indexOf(card1.getFace()) > faceList.indexOf(card2.getFace())) {
+          return -1;
+        } else if (faceList.indexOf(card1.getFace()) == faceList.indexOf(card2.getFace())) {
+          return String.valueOf(card1.getSuit()).compareTo(String.valueOf(card2.getSuit()));
+        }
+        return 0;
+      }
+    }
+
+    @Override
+    public String toString() {
+      return this.face + " of " + this.suit;
+    }
   }
 
-  enum Face {
-    Ace, Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King;
-  }
-
-  enum Suit {
-    SPADES, HEARTS, CLUBS, DIMONDS
-  }
 
 }

387121fe29e1764c09234fba13e97753cfa143a7
evgeniy.voronkin <evgeniy.voronkin@gorparkovki.ru>
2019-07-08 09:43:27 +0300

    file reader

--- src/test/java/ru/simple/java/test/IO/IOLauncher.java
+++ src/test/java/ru/simple/java/test/IO/IOLauncher.java
@@ -4,17 +4,32 @@ import ru.simple.java.test.lists.map.treemap.AverageStudentGrade;
 import ru.simple.java.test.lists.map.treemap.SubjectGrade;
 import ru.simple.java.test.lists.map.treemap.TreeMapLauncher;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.NavigableMap;
 import java.util.Set;
 
 public class IOLauncher {
 
-  public static void main(String[] args) {
+  public static final String FILE_NAME = "GradeBook.txt";
+
+  public static void main(String[] args) throws IOException {
     NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapLauncher.createGrades();
 
-    try (FileWriter fileWriter = new FileWriter("GradeBook.txt")) {
+    readFile(grades);
+
+    readFile();
+  }
+
+  private static void readFile() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      System.out.println(line);
+    }
+  }
+
+  private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) {
+    try (PrintWriter fileWriter = new PrintWriter(new FileWriter(FILE_NAME))) {
       // fileName - передаём название файла, куда будем записывать наши данные
       for (AverageStudentGrade grade : grades.keySet()) {
         // читаем данные из Map и записываем их в файл
@@ -27,7 +42,6 @@ public class IOLauncher {
     } catch (IOException e) {
       e.printStackTrace();
     }
-
   }
 
 }

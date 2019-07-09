1a87a4c6bfd9cdacbe9139953d183cb9481ce4cf
zhenya voronkin <zhenya.voronkin@gmail.com>
2019-07-09 23:23:02 +0300

    byte stream

--- src/test/java/ru/simple/java/test/IO/IOLauncher.java
+++ src/test/java/ru/simple/java/test/IO/IOLauncher.java
@@ -5,6 +5,8 @@ import ru.simple.java.test.lists.map.treemap.SubjectGrade;
 import ru.simple.java.test.lists.map.treemap.TreeMapLauncher;
 
 import java.io.*;
+import java.util.Formatter;
+import java.util.InputMismatchException;
 import java.util.NavigableMap;
 import java.util.Set;
 
@@ -15,9 +17,39 @@ public class IOLauncher {
   public static void main(String[] args) throws IOException {
     NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapLauncher.createGrades();
 
-    readFile(grades);
-
+    /*writeFile(grades);
     readFile();
+    byteStream();*/
+
+    Formatter formatter = new Formatter("BankAccounts.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("please enter clientId, client name, client surname, account balance");
+    int i = 0;
+    while (i < 2) {
+      try {
+        formatter.format("%d, %s, %s, %.2f", Integer.valueOf(reader.readLine()), reader.readLine(), reader.readLine(),
+                Float.valueOf(reader.readLine()));
+        i++;
+      } catch (InputMismatchException e) {
+        System.out.println("Input is incorrect. Please thy again");
+        e.printStackTrace();
+        reader.readLine();
+      }
+      formatter.close();
+    }
+
+  }
+
+  private static void byteStream() throws IOException {
+    try (FileInputStream reader = new FileInputStream(FILE_NAME);
+         FileOutputStream writer = new FileOutputStream("GradeBookByte.txt")) {
+      int c;
+      while ((c = reader.read()) != -1) {
+        System.out.print(c);
+        writer.write(c);
+      }
+    }
   }
 
   private static void readFile() throws IOException {
@@ -28,9 +60,9 @@ public class IOLauncher {
     }
   }
 
-  private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) {
-    try (PrintWriter fileWriter = new PrintWriter(new FileWriter(FILE_NAME))) {
-      // fileName - передаём название файла, куда будем записывать наши данные
+  private static void writeFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) {
+    // fileName - передаём название файла, куда будем записывать наши данные
+    try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(FILE_NAME))) {
       for (AverageStudentGrade grade : grades.keySet()) {
         // читаем данные из Map и записываем их в файл
         fileWriter.write("student: " + grade.getName() + " average grade: " + grade.getAverageGrade() + "\n");

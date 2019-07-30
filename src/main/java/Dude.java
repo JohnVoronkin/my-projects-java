793bab6cc8afd25d24a9d9b23003db9272d068dd
zhenya voronkin <zhenya.voronkin@gmail.com>
2019-07-31 00:17:22 +0300

    dude fix

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,60 +1,48 @@
+import lombok.Getter;
+
+@Getter
 public class Dude {
 
-    private String firstname;
-    private int age;
-    private boolean sex;
+  private String firstName;
+  private int age;
+  private boolean sex;
 
-    public String getFirstname() {
-        return firstname;
+  public static Builder newEntity() {
+    return new Dude().new Builder();
+  }
+
+  public class Builder {
+    private Builder() {
     }
 
-    public int getAge() {
-        return age;
+    public Builder withFirstName(String firstName) {
+      Dude.this.firstName = firstName;
+      return this;
     }
 
-    public boolean isSex() {
-        return sex;
+    public Builder withAge(int age) {
+      Dude.this.age = age;
+      return this;
     }
 
-    public static Builder newEntity() {
-        return new Dude().new Builder();
+    public Builder withSex(boolean sex) {
+      Dude.this.sex = sex;
+      return this;
     }
 
-
-    public class Builder {
-        private Builder() {
-
-        }
-
-        public Builder withFirstName(String firstname) {
-            Dude.this.firstname = firstname;
-            return this;
-        }
-
-        public Builder withAge(int age) {
-            Dude.this.age = age;
-            return this;
-        }
-
-        public Builder withSex(boolean sex) {
-            Dude.this.sex = sex;
-            return this;
-        }
-
-
-        public Dude build() {
-            return Dude.this;
-        }
+    public Dude build() {
+      return Dude.this;
     }
+  }
 
 
-    static Dude dude = Dude.newEntity()
-            .withFirstName("Вася")
-            .withAge(23)
-            .withSex(true).build();
+  static Dude dude = Dude.newEntity()
+          .withFirstName("Вася")
+          .withAge(23)
+          .withSex(true).build();
 
-    public static void main(String[] args) {
-        System.out.println(dude);
-    }
+  public static void main(String[] args) {
+    System.out.println(dude);
+  }
 
 }
